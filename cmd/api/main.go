812fa211ecd8d9058dfae2f5bd1b59273d2cef07
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-12-24 15:44:17 +0700

    feat: Set up OTP request handler and update package name for auth handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/Hdeee1/go-register-login-otp/internal/config"
+	"github.com/Hdeee1/go-register-login-otp/internal/services"
+	"github.com/Hdeee1/go-register-login-otp/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,7 +13,12 @@ func main() {
 	config.LoadEnv()
 	config.InitDatabase()
 
+	otpService := services.NewOTPService(config.DB)
+	authHandler := handlers.NewAuthHandler(otpService)
+
 	r := gin.Default()
+
+	r.POST("api/auth/request-otp", authHandler.RequestOTP)
 
 	log.Fatal(r.Run(":8080"))
 }
